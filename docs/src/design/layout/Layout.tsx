f646f6f1e124e866e6c7bf650ee8a27431c744de
Yannick <yannick.franssen@outlook.com>
2024-02-13 22:25:11 +0100

    Scroll to the top

--- docs/src/design/layout/Layout.tsx
+++ docs/src/design/layout/Layout.tsx
@@ -4,10 +4,12 @@ import { Box } from '@mui/joy';
 
 import Footer from './LayoutElements/Footer';
 import Navbar from './LayoutElements/Navbar';
+import ScrollToTop from '../components/ScrollToTop';
 
 export default function Layout() {
   return (
     <Box sx={{ mt: -11.25 }}>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
