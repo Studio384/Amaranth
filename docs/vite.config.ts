7edb902081f690a5cce24d0d3f2bcfab34719dbd
Yannick <yannick.franssen@outlook.com>
2024-02-14 23:51:14 +0100

    Update artifact workflow

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -20,9 +20,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  optimizeDeps: {
-    include: ['react/jsx-runtime']
-  },
   base: '/Amaranth/',
   envPrefix: 'AMICONS_'
 });
