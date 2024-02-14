65988335e4da7c11193cef05aaecd8bccf503443
Yannick <yannick.franssen@outlook.com>
2024-02-15 00:01:25 +0100

    Update dependencies, and remove redux

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,26 +1,22 @@
-import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
 import { CssBaseline, CssVarsProvider, GlobalStyles } from '@mui/joy';
 
 import joyTheme from './design/joyTheme';
 import router from './Router';
-import store from './store';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <CssVarsProvider theme={joyTheme} defaultMode="system">
-        <CssBaseline />
-        <GlobalStyles
-          styles={{
-            '*': {
-              scrollBehavior: 'smooth'
-            }
-          }}
-        />
-        <RouterProvider router={router} />
-      </CssVarsProvider>
-    </Provider>
+    <CssVarsProvider theme={joyTheme} defaultMode="system">
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          '*': {
+            scrollBehavior: 'smooth'
+          }
+        }}
+      />
+      <RouterProvider router={router} />
+    </CssVarsProvider>
   );
 }
